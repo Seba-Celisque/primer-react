3cb1fb1049d49b09fca8f6716a1516ce8d97ae57
SebaCelisque <seba.celisque@gmail.com>
2023-06-01 09:23:40 -0300

    Primer react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
-  return <h1>Estoy en App</h1>;
-}
+import Navbar from "./components/layaout/navbar/Navbar";
+import { ProducList } from "./components/pages/ItemListContainer/ProducstList";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <ProducList />
+    </div>
+  );
+};
 
 export default App;
